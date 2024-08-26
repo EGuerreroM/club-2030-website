87b1febacd757b0bcd00ba9c43c44094dc4a376c
Enrique Guerrero <bgm94@hotmail.com>
2024-08-25 18:09:36 -0600

    🔧

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,22 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      xxs: '320px',
+      xs: '425px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
     extend: {
       colors: {
         'brand-yellow': '#FEDD00',
         'brand-red': '#DA291C',
         'brand-blue': '#0019AD',
         red: { 400: '#E7584D', 500: '#E43F33', 600: '#DA291C' },
-        blue: {
+        'club-blue': {
           100: '#D1D6FA',
           200: '#9FAAF8',
           300: '#6A7CF7	',
