5aab115004abf475fc33bd8f21f2e070f058d082
Enrique Guerrero <bgm94@hotmail.com>
2024-09-11 23:03:48 -0600

    Refactor appwrite.ts to include createProject function

--- src/services/appwrite.ts
+++ src/services/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from 'appwrite';
+import { Client, Databases, ID, Query } from 'appwrite';
 
 const { APPWRITE_API_ENDPOINT, APPWRITE_API_KEY, APPWRITE_DATABASE_ID, APPWRITE_PROJECT_ID, PROJECTS_COLLECTION_ID } =
   import.meta.env;
@@ -26,6 +26,11 @@ type Project = {
   organization: string | null;
 };
 
+export type LocalProject = Omit<
+  Project,
+  '$id' | '$collectionId' | '$databaseId' | '$createdAt' | '$updatedAt' | '$permissions'
+>;
+
 const getAllProjects = async ({ perPage = 3, page = 1 }: GetAllProjects) => {
   const results = await appwriteDatabase.listDocuments<Project>(APPWRITE_DATABASE_ID, PROJECTS_COLLECTION_ID, [
     Query.orderDesc('$createdAt'),
@@ -36,10 +41,43 @@ const getAllProjects = async ({ perPage = 3, page = 1 }: GetAllProjects) => {
   return results;
 };
 
+const createProject = async (project: LocalProject) => {
+  const id = ID.unique();
+
+  try {
+    const createdProject = await appwriteDatabase.createDocument<Project>(
+      APPWRITE_DATABASE_ID,
+      PROJECTS_COLLECTION_ID,
+      id,
+      project,
+    );
+    console.log(createdProject);
+
+    if (!createdProject) {
+      throw new Error('Error creating project');
+    }
+
+    return new Response(
+      JSON.stringify({
+        message: 'Success!',
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: 'Error creating project',
+      }),
+      { status: 400 },
+    );
+  }
+};
+
 const appwrite = {
   client: appwriteClient,
   database: appwriteDatabase,
   getAllProjects,
+  createProject,
 };
 
 export default appwrite;
